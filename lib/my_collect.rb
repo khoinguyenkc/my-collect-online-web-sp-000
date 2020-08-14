d8d128aa4f42156b1873d45666469d96f3f4bd90
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-14 22:21:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,12 @@
-def sayhi
-  puts "hiiii"
+require 'pry'
+def my_collect(array)
+  i = 0
+  newarray = []
+  while i < array.length 
+    newarray << yield(array[i])
+  end
+  newarray
+  binding.pry
 end
 
-sayhi
+puts my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |language| language.upcase }
